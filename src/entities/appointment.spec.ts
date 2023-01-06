541e31219efc5148b7d924d37ced092b45c2f03e
Wesley Bernardes <bw3sley@gmail.com>
2023-01-05 22:36:00 -0300

    :sparkles: feat: create new tests to get future date and validate dates

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from "vitest";
+
 import { Appointment } from "./appointment";
 
-test("create an appointment", () => {
-    const startsAt = new Date();
-    const endsAt = new Date();
+import { getFutureDate } from "../tests/utils/get-future-date";
 
-    endsAt.setDate(endsAt.getDate() + 1)
+test("create an appointment", () => {
+    const startsAt = getFutureDate("2023-01-05");
+    const endsAt = getFutureDate("2023-01-06");
 
     const appointment = new Appointment({
         customer: "John Doe",
@@ -18,10 +19,24 @@ test("create an appointment", () => {
 })
 
 test("cannot create an appointment with end date before start date", () => {
+    const startsAt = getFutureDate("2023-01-06");
+    const endsAt = getFutureDate("2023-01-05"); 
+
+    expect(() => {
+        return new Appointment({
+            customer: "John Doe",
+            startsAt,
+            endsAt
+        })
+    }).toThrow()
+})
+
+test("cannot create an appointment with start date before now", () => {
     const startsAt = new Date();
     const endsAt = new Date();
 
-    endsAt.setDate(endsAt.getDate() - 1)
+    startsAt.setDate(startsAt.getDate() - 1);
+    endsAt.setDate(endsAt.getDate() + 3);
 
     expect(() => {
         return new Appointment({
---
+++ src/tests/utils/get-future-date.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+
+import { getFutureDate } from "./get-future-date";
+
+test("increases date with one year", () => {
+    const year = new Date().getFullYear();
+
+    expect(getFutureDate(`${year}-01-05`).getFullYear()).toEqual(2024);
+})
